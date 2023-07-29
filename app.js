e47cdcd63368c0fa49e47c62da247a4b5c4c1147
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-30 00:16:02 +0530

    feat: initial bootstrap

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+// 2) Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;

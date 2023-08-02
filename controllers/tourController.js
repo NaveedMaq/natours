72da2ed09e8e1bf1180f1652244a42e1e311ff79
NaveedMaq <naveedmaqbool30@gmail.com>
2023-08-02 10:49:04 +0530

    feat: Add pagination to tours resource

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,18 @@ exports.getAllTours = async (req, res) => {
       query.select('-__v');
     }
 
+    // 4) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 

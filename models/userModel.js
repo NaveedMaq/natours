7952df02f43b15627fa06acc9d271f69f5b8ea9a
NaveedMaq <naveedmaqbool30@gmail.com>
2023-08-03 18:00:25 +0530

    feat: Add user signup + login

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minLength: 8,
+    select: false,
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must have a passwordConfirm'],
+
+    validate: {
+      // This only works with CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.checkPassword = function (candidatePassword, userPassword) {
+  return bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;

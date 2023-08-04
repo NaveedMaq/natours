b6df0a489b6322e6b379b9dd34e1e10aac4898da
NaveedMaq <naveedmaqbool30@gmail.com>
2023-08-04 20:16:35 +0530

    checkpoint: Temporarily pausing development

--- models/tourModel.js
+++ models/tourModel.js
@@ -93,6 +93,33 @@ const toursSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: {

294f28339b3d8f08a49a1cf0f5487a374a3f83f4
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-02 16:44:13 +0530

    complete GET /me route

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,12 @@ const filterObj = (obj, ...allowedFields) => {
 
   return newObj;
 };
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTes password data
   if (req.body.password || req.body.passwordConfirm)

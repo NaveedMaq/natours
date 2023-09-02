c56b5090df6fb004b56f29a50c942149dddae60b
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-02 17:00:18 +0530

    add auth to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,23 +6,19 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword,
-);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// Routes beyond this can only be accessed by authenticated users
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Routes beyond this can only be accessed by admins
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')

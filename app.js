73a9e5d5a22093a98365747e6fd9192a7474455e
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-08 00:01:00 +0530

    add views router

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -73,13 +74,7 @@ app.use((req, res, next) => {
 });
 
 // 2) Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);

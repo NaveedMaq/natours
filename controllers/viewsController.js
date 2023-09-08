9ce0ab2acbc068df265c1b6fbbd129a98e6de53f
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-08 13:16:26 +0530

    add map to tour details page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -23,8 +23,14 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // 2) Build template
 
   // 3) Render template
-  res.status(200).render('tour', {
-    title: tour.name,
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;",
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
 });

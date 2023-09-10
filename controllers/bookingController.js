29f78efab68feb4931a0004f31cc26dafa14a787
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-09-10 12:51:49 +0530

    add feature to create bookings record in the db on payment success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,9 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-// const AppError = require('../utils/appError');
 // const factory = require('./handlerFactory');
-// const APIFeatures = require('../utils/apiFeatures');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
@@ -28,17 +27,28 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
     mode: 'payment',
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
   });
-
-  console.log({ session });
 
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
